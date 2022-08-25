13ee20dcb80c79b04cd7ee3c7dcadec774b7a172
Argamac <edivlev@yandex.ru>
2022-08-26 01:24:41 +0300

    NON

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,25 @@ import java.util.Scanner;
 import static java.lang.System.*;
 
 public class Main {
-    public static void main(String[] args) {
+    private static String scan;
+
+    public static void main(String[] args) {       
+        Main.calc(scan);
+    }
+    public static String calc(String input) {
+        //public static void main(String[] args){
         Scanner scan = new Scanner(in);
-        int a , b, res;
-        out.println("Р’РІРµРґРёС‚Рµ РІС‹СЂР°Р¶РµРЅРёРµ РІ СЃР»РµРґСѓСЋС‰РµРј С„РѕСЂРјР°С‚Рµ: 1+2 РёР»Рё V+X");
+        int a, b, res;
+        out.println("Введите выражение в следующем формате: 1+2 или V+X");
         String oper = scan.nextLine();
         String[] op1 = oper.split("[/*+-]");
         String str = String.join("", op1);
-        String[] op2 = str.split("[\"I\", \"II\", \"III\", \"IV\", \"V\", \"VI\", \"VII\", \"VIII\", \"IX\", \"X\"]");
+        String[] op2 = str.split("[\"0\",\"I\", \"II\", \"III\", \"IV\", \"V\", \"VI\", \"VII\", \"VIII\", \"IX\", \"X\"]");
         String str1 = String.join("", op2);
         int len = str1.length();
         //out.println(op1.length);
         //out.println(op2[1]);
-        //out.println(len);
+        //out.println(str1);
         String[] roman;
         roman = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                 "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
@@ -23,11 +29,10 @@ public class Main {
                 "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
                 "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
                 "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
-
-        if (op1.length > 2 |  len == 3 | len == 1 | len > 4) {
-            out.println("РћС€РёР±РєР°: РќРµ РІРµСЂРЅРѕ РІРІРµРґРµРЅРѕ РІС‹СЂР°Р¶РµРЅРёРµ");
+        if (op1.length > 2 | len == 3 | len == 1 | len > 4) {
+            out.println("Ошибка: Не верно введено выражение");
         } else {
-                    if (op1[0].equals("I")) {
+            if (op1[0].equals("I")) {
                 a = 1;
             } else if (op1[0].equals("II")) {
                 a = 2;
@@ -71,25 +76,25 @@ public class Main {
                 b = 10;
             } else
                 b = Integer.parseInt(op1[1]);
-            if (a == 0 | b == 0 | a > 10 | b >10) {
-                out.println("РћС€РёР±РєР°: Р’РІРѕРґРёРјС‹Рµ С‡РёСЃР»Р° РґРѕР»Р¶РЅС‹ Р±С‹С‚СЊ Р±РѕР»СЊС€Рµ 1 Рё РЅРµ Р±РѕР»СЊС€Рµ 10");
-            }else if (oper.contains("/")) {
-                 if (len == 0) {
+            if (a == 0 | b == 0 | a > 10 | b > 10) {
+                out.println("Ошибка: Вводимые числа должны быть больше 1 и не больше 10");
+            } else if (oper.contains("/")) {
+                if (len == 0) {
                     res = a / b;
                     out.println(roman[res]);
-                }
-                else out.print(a / b);
+                } else out.print(a / b);
             }
                 if (oper.contains("*")) {
                     if (len == 0) {
                         res = a * b;
                         out.println(roman[res]);
-                    }
-                    else out.print(a * b);
+                    } else out.print(a * b);
                 }
                 if (oper.contains("-")) {
                     if (len == 0 & a < b) {
-                        out.println("РћС€РёР±РєР°: Р’ СЂРёРјСЃРєРѕР№ СЃРёСЃС‚РµРјРµ РЅРµС‚ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅС‹С… С‡РёСЃРµР»");
+                        out.println("Ошибка: В римской системе нет отрицательных чисел");
+                    } else if (a == b) {
+                        out.println("Ошибка: Результат меньше 1");
                     } else if (len == 0) {
                         res = a - b;
                         out.println(roman[res]);
@@ -99,9 +104,10 @@ public class Main {
                     if (len == 0) {
                         res = a + b;
                         out.println(roman[res]);
-                    }
-                    else out.print(a + b);
+                    } else out.print(a + b);
                 }
-        }
+            }
+        return input;
+
     }
 }
